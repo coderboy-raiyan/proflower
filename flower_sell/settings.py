97cb3af33a9a1614ea716f0b60086ef24eddb1e6
Raiyan <tajkierhaque@gmail.com>
2024-01-18 15:50:17 +0600

    allowed host wild card added

--- flower_sell/settings.py
+++ flower_sell/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://flower-sell.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS=["https://flower-sell.onrender.com"]
 
 # Application definition
