eda14d57ca172e909befb356bfcdecedd20ed49c
Raiyan <tajkierhaque@gmail.com>
2024-01-18 15:40:03 +0600

    requirements txt added

--- flower_sell/settings.py
+++ flower_sell/settings.py
@@ -35,8 +35,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS=[]
+ALLOWED_HOSTS = ["https://flower-sell.onrender.com"]
+CSRF_TRUSTED_ORIGINS=["https://flower-sell.onrender.com"]
 
 # Application definition
 
